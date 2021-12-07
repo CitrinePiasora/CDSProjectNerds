cb36ca3f8c2e711bafc4d472379cadb37f4f71c2
Fauzan Ardhana <31564867+fauzanardh@users.noreply.github.com>
2021-12-07 16:07:57 +0700

    update documentation

--- main.py
+++ main.py
@@ -10,7 +10,34 @@ from utils.beatmap import Beatmap
 from utils.predict import predict_map_type
 
 
-app = FastAPI()
+description = "OsuClassy is a beatmap classifier that uses machine learning to classify beatmaps."
+tags_metadata = [
+    {
+        "name": "predict",
+        "description": "Predict beatmap type.",
+    },
+    {
+        "name": "beatmaps",
+        "description": "Beatmap information, such as beatmap ID, title, creator, etc. Also includes the predictions!",
+    }
+]
+app = FastAPI(
+    title="OsuClassy",
+    description=description,
+    version="0.0.1",
+    terms_of_service="http://localhost.com/terms",
+    contact={
+        "name": "Fauzan Ardhana",
+        "email": "fauzanardh@gmail.com",
+    },
+    license_info={
+        "name": "MIT License",
+        "url": "https://opensource.org/licenses/MIT",
+    },
+    openapi_tags=tags_metadata,
+    docs_url="/docs",
+    redoc_url=None,
+)
 classification_model = OsuClassifier(
     MAP_INFO_FEATURES,
     HIT_OBJECTS_FEATURES,
@@ -24,19 +51,22 @@ classification_model = OsuClassifier(
 classification_model.eval()
 classification_model.load_state_dict(torch.load("model/pretrained_weights/osuclasification_best.pt"))
 
-@app.get("/", response_model=Response)
+@app.get("/", tags=["beatmaps"], response_model=Response)
 async def root():
     return Response(
         code=SUCCESS,
         message="Welcome to OsuClassy API!",
     )
 
-@app.post("/predict", response_model=Response)
-async def predict(file: UploadFile=File(...)):
+@app.post("/predict", tags=["predict"], response_model=Response)
+async def predict_map(file: UploadFile=File(...)):
     """
     Predict beatmap class.
-    """
 
+    - **file**: .osu file to predict.
+    \f
+    :param file: Uploaded file.
+    """
     async with tempfile.TemporaryFile(mode="w+") as f:
         content = await file.read()
         # Decode and remove carriage return for beatmap saved on windows
@@ -61,22 +91,3 @@ async def predict(file: UploadFile=File(...)):
         }
     )
     return response
-
-def custom_openapi():
-    """
-    Custom OpenAPI function to add custom info to the OpenAPI document.
-    """
-    if app.openapi_schema:
-        return app.openapi_schema
-    openapi_schema = get_openapi(
-        title="OsuClassy API",
-        version="0.0.1",
-        description="Open source API for Osu beatmap classifier.",
-        routes=app.routes,
-    )
-    openapi_schema["info"]["x-logo"] = {
-        "url": "https://i.ppy.sh/84650abbb8bc7fcbfa58f6941ac6c2d00ef4a5bd/68747470733a2f2f6f73752e7070792e73682f77696b692f696d616765732f4272616e645f6964656e746974795f67756964656c696e65732f696d672f75736167652d73696e676c652d636f6c6f75722e706e67"
-    }
-    app.openapi_schema = openapi_schema
-    return openapi_schema
-app.openapi = custom_openapi
