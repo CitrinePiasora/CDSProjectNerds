5baa6724b89ee084531d17460d11dce04b82575d
Fauzan Ardhana <31564867+fauzanardh@users.noreply.github.com>
2021-12-18 19:51:27 +0700

    chore: remove unused components

--- frontend/src/components/DarkModeToggle.tsx
+++
@@ -1,7 +0,0 @@
-import { useColorMode, Switch } from "@chakra-ui/react";
-
-export const DarkModeSwitch = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
-  const isDark = colorMode === "dark";
-  return <Switch top="10px" isChecked={isDark} onChange={toggleColorMode} />;
-};
