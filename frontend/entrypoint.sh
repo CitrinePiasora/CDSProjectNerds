9b57d1ae5f3b3a864d350091a8b38072f1f06c43
Fauzan Ardhana <31564867+fauzanardh@users.noreply.github.com>
2021-12-19 00:14:29 +0700

    feature: add dockerfile for the frontend

---
+++ frontend/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Build the frontend
+echo "Building the frontend..."
+npm run build
+
+# Run the frontend
+echo "Running the frontend..."
+npm run start
--- start.sh
+++ start.sh
@@ -3,15 +3,19 @@
 # Create a new namespace
 kubectl create namespace osuclassy-dev
 
-# Database creation
+# Creating necessary stuff
+kubectl apply -f k8s_configurations/secrets.yaml
+kubectl apply -f k8s_configurations/pv.yaml
+kubectl apply -f k8s_configurations/configmaps.yaml
+
 echo "Deploying database..."
-kubectl apply -f k8s_configurations/db-secret.yaml
-kubectl apply -f k8s_configurations/db-pv.yaml
-kubectl apply -f k8s_configurations/db-configmap.yaml
 kubectl apply -f k8s_configurations/db-deployment.yaml
 
 echo "Deploying the backend..."
 kubectl apply -f k8s_configurations/backend-deployment.yaml
+
+echo "Deploying the frontend..."
+kubectl apply -f k8s_configurations/frontend-deployment.yaml
 
 echo "Creating ingress..."
 kubectl apply -f k8s_configurations/osuclassy-ingress.yaml
