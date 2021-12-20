97ad3b48b61ce8aeaaa552a3221e8b55a8d4142d
Fauzan Ardhana <31564867+fauzanardh@users.noreply.github.com>
2021-12-20 16:00:40 +0700

    feature: add router progress bar

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,15 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, useColorModeValue } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import NextNProgress from "nextjs-progressbar";
 
 import theme from "../theme";
 import { NavBar } from "../components/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const mainColor = useColorModeValue("#ff5ea3", "#ff94c4");
   return (
     <ChakraProvider resetCSS theme={theme}>
+      <NextNProgress color={mainColor} options={{ showSpinner: false }} />
       <NavBar />
       <Component {...pageProps} />
     </ChakraProvider>
